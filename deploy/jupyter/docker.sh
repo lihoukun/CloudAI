c03b56916e46aaa479650ff0cad71bf74413501c
Houkun Li <lihoukun@gmail.com>
2018-04-20 13:35:52 -0700

    user /group

--- deploy/jupyter/docker.sh
+++ deploy/jupyter/docker.sh
@@ -1,3 +1,3 @@
 docker container kill exaai-jupyter-notebook
 docker container rm exaai-jupyter-notebook
-docker run --name exaai-jupyter-notebook -d -p 30088:8888 -v /nfs/nvme/notebooks/work:/home/jovyan/work exaai/jupyter-notebook start-notebook.sh --NotebookApp.token=''
+docker run -e NB_UID=1000 -e NB_GID=1000 -e GRANT_SUDO=yes --name exaai-jupyter-notebook -d -p 30088:8888 -v /nfs/nvme/notebook/work:/home/jovyan/work exaai/jupyter-notebook start-notebook.sh --NotebookApp.token=''
