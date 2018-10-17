0057355dadcb57cbf0f00078918bad32282e96f4
Houkun Li <lihoukun@gmail.com>
2018-10-16 17:31:07 -0700

    move jupyter start to home

--- tools/deploy/docker.py
+++ tools/deploy/docker.py
@@ -7,7 +7,7 @@ def stop_jupyter():
 def start_jupyter():
     cmd = "docker run -e NB_UID={} -e NB_GID={} -e GRANT_SUDO=yes".format(os.environ.get('JUPYTER_UID'), os.environ.get('JUPYTER_GID'))
     cmd += " --name exaai-jupyter-notebook -d -p {}:8888".format(os.environ.get('JUPYTER_PORT'))
-    cmd += " -v {}/notebook/work:/home/jovyan/work".format(os.environ.get('SHARED_HOST'))
+    cmd += " -v {}/notebook:/home/jovyan".format(os.environ.get('SHARED_HOST'))
     cmd += " exaai/jupyter-notebook start-notebook.sh --NotebookApp.token=''"
     print(cmd)
     os.system(cmd)
